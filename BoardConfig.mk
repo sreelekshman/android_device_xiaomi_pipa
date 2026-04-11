9b537225540f09a9f8565a47efcc3773e66c68a9
swiitchOFF <120115258+swiitchOFF@users.noreply.github.com>
2026-04-11 09:39:25 +0530

    pipa: Move c2_manifest_vendor.xml to device
    Change-Id: I59ceeec26346c0a04a636fa840218dd75b703b7b
    Signed-off-by: sreelekshman <sreelekshmanchunakara@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -228,6 +228,8 @@ DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += \
 DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/manifest.xml
 DEVICE_MATRIX_FILE += hardware/qcom-caf/common/compatibility_matrix.xml
 
+DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/hidl/c2_manifest_vendor.xml
+
 # Wi-Fi
 BOARD_WLAN_DEVICE := qcwcn
 BOARD_HOSTAPD_DRIVER := NL80211
