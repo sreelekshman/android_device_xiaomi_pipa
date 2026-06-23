5eff4c630fb25bd0c2a2eb9463a976da43a8a2ad
Jis G Jacob <studiokeys@blissroms.org>
2026-06-23 15:25:26 +0530

    pipa: Update A/B flags according to CP2A changes
    Signed-off-by: Jis G Jacob <studiokeys@blissroms.org>

--- device.mk
+++ device.mk
@@ -20,7 +20,7 @@ PRODUCT_AAPT_PREF_CONFIG := xxxhdpi
 # A/B
 ifeq ($(TARGET_IS_VAB),true)
 # Inherit virtual_ab_ota product
-$(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota/launch_with_vendor_ramdisk.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota/compression.mk)
 
 PRODUCT_PACKAGES += \
     android.hardware.boot-service.qti \
